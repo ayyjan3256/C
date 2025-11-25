ec3cf9add2208e2f5c2f9d0a2d9d67f3f694e509
Ayyub Murjan <125450661+ayyjan3256@users.noreply.github.com>
2025-11-25 17:14:07 +0300

    Update Exercise1.17.c

--- 1.9 Character Array/Exercise1.17.c
+++ 1.9 Character Array/Exercise1.17.c
@@ -1,1 +1,27 @@
-
+#include <stdio.h>
+#define MAX 100
+int get_line(char line[],int limit);
+int main(){
+	int max,length;
+	char line[MAX];
+	while((length=get_line(line,MAX))>0){
+		if (length>80){
+    printf("%s\n",line);
+      }
+		}
+	return 0;
+	}
+int get_line(char line[],int limit){
+	int i,c;
+	i=0;
+	while((c=getchar())!=EOF&&c!='\n'&&i<limit-1){
+	  line[i]=c;
+		i++;
+	}
+    if (c=='\n'){
+		line[i]=c;
+		i++;
+		}
+	line[i]='\0';
+	return i;
+}
