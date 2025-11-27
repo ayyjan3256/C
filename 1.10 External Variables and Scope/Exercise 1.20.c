d416cd57c4ea8cca8f06c1e3cb41a12f399cce93
Ayyub Murjan <125450661+ayyjan3256@users.noreply.github.com>
2025-11-27 23:26:04 +0300

    Update Exercise 1.20.c

--- 1.10 External Variables and Scope/Exercise 1.20.c
+++ 1.10 External Variables and Scope/Exercise 1.20.c
@@ -1,1 +1,24 @@
 
+#include <stdio.h>
+#define TABSIZE 8
+void detab(char character);
+int main(){
+	int c;
+	char column;
+	while((c=getchar())!=EOF){
+			if (c=='\t'){
+			int spaces=TABSIZE-(column%TABSIZE);
+				for (int j=0;j<spaces;++j){
+					putchar(' ');
+					}
+				column+=spaces;
+				}
+			else{
+				putchar(c);
+				column++;
+				if(c=='\n'){
+					column=0;
+					}
+				}
+		}
+	}
