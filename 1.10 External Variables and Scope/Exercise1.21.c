efc112d9ee20ab4116398e4bc4dc46f074447cc8
Ayyub Murjan <125450661+ayyjan3256@users.noreply.github.com>
2025-12-01 16:13:03 +0300

    Refactor whitespace handling in Exercise1.21.c
    Refactor whitespace handling to improve tab and space output.

--- 1.10 External Variables and Scope/Exercise1.21.c
+++ 1.10 External Variables and Scope/Exercise1.21.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 #define TABSIZE 8
 int main(){
-	int c,column,spaces;
-	spaces=column=0;
-	while((c=getchar())!=EOF){
-		if (c==' '){
-            spaces++;
-			column++;
-            if ((column + spaces) % TABSIZE == 0) {
-				putchar('\t');
-				spaces = 0;  
-				column += TABSIZE - (column % TABSIZE);
-                }
-            }
-        else{
-            while (spaces > 0) {
-                putchar(' ');
-                spaces--;
-                }
-            putchar(c);
-            if(c=='\n'){
-            column=0;
-                }
-            else{
-            column++;
-                }
-            }
+ int c,column,spaces;
+ spaces=column=0;
+ while((c=getchar())!=EOF){
+      if (c==' '){
+           spaces++;
+           column++;
+           if((column%TABSIZE)==0){
+               putchar('\t');
+               putchar('t');
+               spaces=0;
+           }
+      }
+     else{
+        while(spaces>0){
+               putchar(' ');
+               spaces--;
+               putchar('s');
+             }
+         if(c=='\n'){
+             column=0;
+         }
+         else{
+             putchar(c);
+             column++;
+       }
     }
-    return 0;
+  }  
+return 0;
 }
