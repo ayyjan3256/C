deaec81077da7e689d04aaf2e87fab09d49d3300
Ayyub Murjan <125450661+ayyjan3256@users.noreply.github.com>
2026-02-22 11:16:36 +0300

    Optional 0x/0X infront of the hex input

--- Chapter 2/Exercise2.3.c
+++ Chapter 2/Exercise2.3.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 int httoi(char numbers[]);
 int main(){
-	char string0[]={"0xfff"};
+	char string0[]={"0x1f"};
 	printf("%d",httoi(string0));
 	return 0;
 	}
 int httoi(char numbers[]){
 	int n=0;
-	int i,k;
-	for (i=0;numbers[i]!='\0';i++){
-		;
-		}
-	
-	for(int j=2;j<i;j++){
+	int i,j,k;
+    for(i=0;numbers[i]!='\0';i++){
+        ;
+    }
+	if (numbers[0]=='0'&&(numbers[1]=='x'||numbers[1]=='X')){
+        j=2;
+    }
+    else{
+        j=0;
+    }
+	for(;j<i;j++){
 		if (numbers[j]>='0' && numbers[j]<='9'){
 			k=numbers[j]-'0';
 			}
