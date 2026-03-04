22bafbcb5a23fca04210dd3588ba784dbabe2702
Ayyub Murjan <125450661+ayyjan3256@users.noreply.github.com>
2026-03-04 13:50:27 +0300

    Add unescape function and modify main for testing

--- Chapter 3/Exercise3.2.c
+++ Chapter 3/Exercise3.2.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 void escape(char s[],char t[]);
+void unescape(char s[], char t[]);
 int main(){
-    char s[] = "Start\tColumn1\tColumn2\nNext Line with a backslash: \\";
+    char s1[] = "Start\tColumn1\tColumn2\nNext Line with a backslash:\\";
+    char s2[] = "Start\\tColumn1\\tColumn2\\nNext Line with a backslash:\\\\";
     char t[1000];
-    escape(s,t);
-    printf("Original:%s\n",s);
-    printf("Escaped:%s",t);
+    escape(s1,t);
+    printf("Original:%s\n",s1);
+    printf("Escaped:%s\n",t);
+    unescape(s2,t);
+     printf("Original:%s\n",s2);
+    printf("Unescaped:%s",t);
     return 0;
 }
 void escape(char s[], char t[]) {
@@ -34,4 +39,31 @@ void escape(char s[], char t[]) {
         }
     }
     t[i] = '\0';
+}
+
+void unescape(char s[], char t[]){
+    int i = 0;
+    int j = 0;
+    for(int k=0;s[k]!='\0';k++){
+        switch(s[k]){
+            case '\\':
+              switch(s[++k]){
+                case 'n':
+                    t[j++]='\n';
+                    k++;
+                    break;
+                case 't':
+                    t[j++]='\t';
+                    k++;
+                    break;
+                case '\\':
+                    t[j++]='\\';
+                    k++;
+                    break;
+              }
+        default:
+            t[j++]=s[k];
+        }
+    }
+    t[j] = '\0';
 }
